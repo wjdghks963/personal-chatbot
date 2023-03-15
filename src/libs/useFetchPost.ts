2df6732c86b31e2620519db9472ff8749efa9903
wjdghks963 <chsw000@naver.com>
2023-03-15 21:44:06 +0900

    added :  - Post fetch 하는 lib

---
+++ src/libs/useFetchPost.ts
@@ -0,0 +1,32 @@
+import {SettingDataJson} from "../../type";
+
+interface ApiFetch<G> {
+    url:string,
+    json:G,
+}
+
+export default async function useFetchPost<T, G>(params: ApiFetch<G>){
+    try {
+        const response = await fetch(`/api/${params.url}`, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(params.json),
+        });
+
+        const data = await response.json();
+
+        if (response.status !== 200) {
+            throw data.error || new Error(`Request failed with status ${response.status}`);
+        }
+
+        return data as T
+
+    }catch(error:any) {
+        console.error(error);
+        alert(error.message);
+    }
+
+
+}
