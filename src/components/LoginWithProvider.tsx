a5fd4d794fad1d4235309822df910c5a0f93ce01
wjdghks963 <chsw000@naver.com>
2023-03-16 21:52:12 +0900

    modified :  - login 가능한 inputbox

---
+++ src/components/LoginWithProvider.tsx
@@ -0,0 +1,12 @@
+import {ProviderMapObject} from "../../pages/signIn";
+import Image from "next/image";
+
+export default function LoginWithProvider({providerMap, providerKey}:{providerMap?:ProviderMapObject, providerKey:string}){
+
+    return (
+        <div className={`flex p-5 cursor-pointer border-2 border-black rounded-md shadow-md ${providerMap?.bgColor}`} onClick={providerMap?.signinFn}>
+            <Image className={'mr-5'} src={providerMap?.svg?.src as string } alt={`${providerKey}`} width={25} height={25}/>
+            <span className={"font-semibold"}>{providerKey}</span>
+            <span>로 시작하기</span>
+    </div>)
+}
