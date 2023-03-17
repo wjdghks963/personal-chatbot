b8169c59a00b5ae4a2dc3dd876f4a33fc58ff90a
wjdghks963 <chsw000@naver.com>
2023-03-17 20:19:58 +0900

    added :  - Chat bubble

---
+++ src/components/chat/ChatBubble.tsx
@@ -0,0 +1,32 @@
+import {ChatObject, SettingDataJson} from "../../../type";
+
+
+export default function ChatBubble({role, content}:ChatObject){
+
+     const item : SettingDataJson = JSON.parse(localStorage.getItem('settingDataJson') ?? `{
+         concepts: "",
+         frequencyPenalty: 0,
+         presencePenalty: 0,
+         topP: 0,
+         aiName: "나", userName: "AI" }`) ;
+
+
+
+    const name = role === 'user' ? item.userName : item.aiName;
+
+    const bgByRole = (role:'user'|'assistant'):string =>{
+        switch (role){
+            case 'user': {
+                return 'bg-blue-100'
+            }
+            case 'assistant':{
+                return 'bg-blue-300'
+            }
+        }
+    }
+
+    return <div className={`flex-col`}>
+        <span className={`block font-semibold mb-3`}>{name}</span>
+        <span className={`block border-blue ${bgByRole(role)} p-2`}>{content}</span>
+    </div>
+}
