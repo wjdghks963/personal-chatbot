4499ef3faa5125a196eb45a5ba3f7b9c54bc0418
wjdghks963 <chsw000@naver.com>
2023-03-17 20:20:28 +0900

    added :  - analytics 추가

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,18 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import {FireBaseAnalytics} from "../src/libs/firebase/Firebase";
+
+
+
 
 function MyApp({ Component, pageProps }: AppProps) {
+  const startAnalytics = async () => {
+    await FireBaseAnalytics()
+  }
+  startAnalytics();
+
   return <Component {...pageProps} />
 }
 
-export default MyApp
+export default MyApp;
+
