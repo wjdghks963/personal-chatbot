05da09fe20d3ec707c4b774b927718bff18501a5
wjdghks963 <chsw000@naver.com>
2023-03-18 23:27:01 +0900

    modified :  - localsetting 로직 추가  - 스타일링 변경

--- pages/profile.tsx
+++ pages/profile.tsx
@@ -5,6 +5,7 @@ import {onAuthStateChanged} from "firebase/auth";
 import { SignIn } from "../src/components/user/SignIn";
 import {getSetting} from "../src/libs/firebase/firestorage";
 import SettingForm from "../src/components/SettingForm";
+import {SettingDataJson} from "../type";
 
 
 export default  function Profile(){
@@ -12,10 +13,23 @@ export default  function Profile(){
     const [formToggle,setFormToggle] = useState<boolean>(false);
     const [name, setName] = useState('유저');
 
-    const getSettingUserName =  () =>{
-        if (!auth.currentUser) return;
-        return getSetting(auth.currentUser?.email as string).then((json)=> setName(json?.userName ?? auth.currentUser?.displayName)
-        );
+    const getSettingUserName = async () =>{
+        const localSettingData:SettingDataJson | ""= JSON.parse(localStorage.getItem('settingDataJson') ?? '{}');
+
+        if(auth.currentUser){
+           try{
+               const result= await getSetting(auth.currentUser?.email ?? '');
+               if(result?.userName === "") {
+                   setName(auth.currentUser.displayName!)
+               }else{
+                   setName(result?.userName);
+               }
+           }catch (e:any){
+               // @ts-ignore
+               setName(localSettingData.userName ?? "유저");
+           }
+        }
+
     }
 
 
@@ -33,17 +47,18 @@ export default  function Profile(){
     return(
 
     <NavBarLayout>
-            <div className={`flex justify-center w-full`}>
-                {formToggle && <SettingForm setFormToggle={setFormToggle}/>}
+        {formToggle && <SettingForm setFormToggle={setFormToggle}/>}
+            <div className={`flex w-full h-full ${formToggle ? 'popupOpen' : null} `}>
                 {!isLoggedIn ? <SignIn/> : (
-                    <div className={`w-full h-[100vh] ${formToggle ? 'popupOpen' : null}`}>
-                        <span className={`block`}>{name}</span>
-                        <span className={`block`}>{name}</span>
-                        <span className={`block`}>{name}</span>
-                        <span className={`block`}>{name}</span>
-
-                        <span onClick={()=>setFormToggle(true)}>설정</span>
-                        <button className={'border-blue p-3 hover:text-white hover:bg-blue-500'} onClick={logout}>로그아웃</button>
+                    <div className={`w-full flex-col space-y-36 text-center flex-grow my-32 `}>
+                        <div className={'flex-col flex-none'}>
+                            <span className={`block font-semibold`}>{name}</span>
+                            <span className={`block font-semibold`}>{auth.currentUser?.email}</span>
+                        </div>
+                        <div className={'flex-col space-y-3'}>
+                            <span className={'block border-blue mx-auto p-3 hover:text-white hover:bg-blue-500 w-1/3'} onClick={()=>setFormToggle(true)}>설정</span>
+                            <button className={'block border-blue mx-auto p-3 hover:text-white hover:bg-blue-500 w-1/3'} onClick={logout}>로그아웃</button>
+                        </div>
                     </div>
                     )}
             </div>
