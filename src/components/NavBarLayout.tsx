30b0b25cb7c1f22186e5c4c013b84dd3abd2d1f2
wjdghks963 <chsw000@naver.com>
2023-03-18 14:04:32 +0900

    modified :  - SEO 추가

--- src/components/NavBarLayout.tsx
+++ src/components/NavBarLayout.tsx
@@ -1,15 +1,21 @@
 import NavBar from "./NavBar";
 import React from "react";
+import Head from "next/head";
+import SEO from "./SEO/Head";
 
 export default function NavBarLayout({children}:{children:React.ReactNode}){
 
 
     return (
+        <>
+        <SEO title={'OA'}/>
         <main className={`h-[100vh] flex-col`}>
+
             <div className={`h-full relative`}>
                 {children}
             </div>
             <NavBar/>
 
-        </main>)
+        </main>
+        </>)
 }
