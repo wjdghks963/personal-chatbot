c2f28a72b59467c40c80f69b67c4bd51c805d6fb
wjdghks963 <chsw000@naver.com>
2023-03-19 21:12:47 +0900

    modified :  - assets 바꿈

--- pages/setting-benefit.tsx
+++ pages/setting-benefit.tsx
@@ -1,8 +1,8 @@
 import Image from "next/image";
-import hasRole from "../src/assets/has-role.png"
-import hasNoRole from "../src/assets/has-no-role.png"
-import strongPanelty from "../src/assets/strong-panelty.png"
-import minusPanelty from "../src/assets/minus-panelty.png"
+import hasRole from "../public/has-role.png"
+import hasNoRole from "../public/has-no-role.png"
+import strongPenalty from "../public/strong-penalty.png"
+import minusPenalty from "../public/minus-penalty.png"
 
 export default function settingBenefit(){
 
@@ -12,11 +12,11 @@ export default function settingBenefit(){
                 <h2 className={'font-extrabold text-3xl mb-10'}>역할</h2>
                 <div className={'w-full'}>
                 <span>역할을 의사로 줬을때</span>
-                    <Image className={'mt-3'} src={hasRole} alt={'역할이 없을때'}/>
+                    <Image className={'mt-3'} src={hasRole} alt={'역할이 없을때'} priority/>
                 </div>
                 <div className={'w-full'}>
                 <span>역할을 아무것도 주지 않았을때</span>
-                <Image className={'mt-3'} src={hasNoRole} alt={'역할이 없을때'}/>
+                <Image className={'mt-3'} src={hasNoRole} alt={'역할이 없을때'} priority/>
                 </div>
             </div>
 
@@ -24,11 +24,11 @@ export default function settingBenefit(){
                 <h2 className={'font-extrabold text-3xl mb-10'}>패널티</h2>
                 <div className={'w-full'}>
                     <span>강력한 패널티를 줬을때</span>
-                    <Image className={'mt-3'} src={strongPanelty} alt={'강력한 패널티'}/>
+                    <Image className={'mt-3'} src={strongPenalty} alt={'강력한 패널티'} priority/>
                 </div>
                 <div className={'w-full'}>
                     <span>패널티를 마이너스로 줬을때</span>
-                    <Image className={'mt-3'} src={minusPanelty} alt={'매우 약한 패널티'}/>
+                    <Image className={'mt-3'} src={minusPenalty} alt={'매우 약한 패널티'} priority/>
                 </div>
             </div>
     </div>)
