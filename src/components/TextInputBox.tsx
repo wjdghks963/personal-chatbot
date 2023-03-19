97cedad6670713dc90958c6d1b2f1e7282305cd8
wjdghks963 <chsw000@naver.com>
2023-03-19 14:54:30 +0900

    refactor :  - 글자수 제한

--- src/components/TextInputBox.tsx
+++ src/components/TextInputBox.tsx
@@ -9,7 +9,7 @@ export default function TextInputBox<T>({propertyRef, identity, placeholder}:Tex
     return (
         <div className={"py-3 flex justify-between"}>
             <span className={"my-auto align-middle"}>{identity}</span>
-            <input className="p-3 bg-inherit border-blue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:border-blue-600 focus:ring-blue-600" type={"text"} ref={propertyRef} placeholder={`${placeholder}`}/>
+            <input className="p-3 bg-inherit border-blue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:border-blue-600 focus:ring-blue-600" type={"text"} maxLength={10} ref={propertyRef} placeholder={`${placeholder}`}/>
         </div>
     )
 
