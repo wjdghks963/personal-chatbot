bf8145e683f822f3af17fc84be304d2fc7e816b9
wjdghks963 <chsw000@naver.com>
2023-03-19 14:52:52 +0900

    refactor :  - hint 더 잘보이게

--- src/components/ValueInputBox.tsx
+++ src/components/ValueInputBox.tsx
@@ -57,10 +57,12 @@ export default function ValueInputBox<T>({propertyRef, identity, range}:ValueInp
                 <input onChange={(e)=>onChange(e)} type={"range"} defaultValue={0} min={range.min} max={range.max} step={0.1} ref={propertyRef}/>
                 <span className={'ml-3'}>{identity}의 정도를 설정해주세요</span>
             </div>
-            <div className={"relative"}>
+            <div className={"relative flex"}>
                 {popup ? <InputDescription title={identity!} description={description()} cssClassName={"top-10 z-10"} />: null}
-
-            <span className={"cursor-help p-3 block "} onMouseEnter={()=>descriptionPopupUp()} onMouseOut={()=>descriptionPopupDown()}> {identity} : {value}%</span>
+                <svg className={"w-6 text-blue-600"} fill="none" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M9.879 7.519c1.171-1.025 3.071-1.025 4.242 0 1.172 1.025 1.172 2.687 0 3.712-.203.179-.43.326-.67.442-.745.361-1.45.999-1.45 1.827v.75M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9 5.25h.008v.008H12v-.008z"></path>
+                </svg>
+            <span className={"cursor-help p-1  block "} onMouseEnter={()=>descriptionPopupUp()} onMouseOut={()=>descriptionPopupDown()}> {identity} : {value}%</span>
             </div>
         </div>
     )
