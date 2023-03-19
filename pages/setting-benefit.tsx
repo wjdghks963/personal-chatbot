4c090863e8cfca1eda244cbef2be0ef92ac2fd6e
wjdghks963 <chsw000@naver.com>
2023-03-19 23:01:28 +0900

    modified :  - image production bug fix 중

--- pages/setting-benefit.tsx
+++ pages/setting-benefit.tsx
@@ -9,11 +9,11 @@ export default function settingBenefit(){
                 <h2 className={'font-extrabold text-3xl mb-10'}>역할</h2>
                 <div className={'w-full '}>
                 <span>역할을 의사로 줬을때</span>
-                    <Image className={'mt-3  mx-auto'} src={'/images/has-role.png'} alt={'역할이 없을때'} width={1000} height={1000} priority/>
+                    <Image className={'mt-3  mx-auto'} src={'/static/images/has-role.png'} alt={'역할이 없을때'} width={1000} height={1000} priority/>
                 </div>
                 <div className={'w-full'}>
                 <span>역할을 아무것도 주지 않았을때</span>
-                <Image className={'mt-3  mx-auto'} src={'/images/has-no-role.png'} alt={'역할이 없을때'} width={1000} height={1000} priority/>
+                <Image className={'mt-3  mx-auto'} src={'/static/images/has-no-role.png'} alt={'역할이 없을때'} width={1000} height={1000} priority/>
                 </div>
             </div>
 
@@ -21,11 +21,11 @@ export default function settingBenefit(){
                 <h2 className={'font-extrabold text-3xl mb-10'}>패널티</h2>
                 <div className={'w-full'}>
                     <span>강력한 패널티를 줬을때</span>
-                    <Image className={'mt-3  mx-auto'} src={'/images/strong-penalty.png'} alt={'강력한 패널티'} width={1000} height={1000} priority/>
+                    <Image className={'mt-3  mx-auto'} src={'/static/images/strong-penalty.png'} alt={'강력한 패널티'} width={1000} height={1000} priority/>
                 </div>
                 <div className={'w-full'}>
                     <span>패널티를 마이너스로 줬을때</span>
-                    <Image className={'mt-3  mx-auto'} src={'/images/minus-penalty.png'} alt={'매우 약한 패널티'} width={1000} height={1000} priority/>
+                    <Image className={'mt-3  mx-auto'} src={'/static/images/minus-penalty.png'} alt={'매우 약한 패널티'} width={1000} height={1000} priority/>
                 </div>
             </div>
     </div>)
