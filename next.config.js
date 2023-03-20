f291b536ce0711b49772e0fd8603726153ec76a5
wjdghks963 <chsw000@naver.com>
2023-03-20 11:46:24 +0900

    modified :  - static image build 파일에선 나오지만 production에서 안나오는 문제 해결중

--- next.config.js
+++ next.config.js
@@ -20,7 +20,6 @@ const nextConfig = {
   },
   images:{
     formats:['image/avif','image/webp'],
-    disableStaticImages: false,
   },
 }
 
--- pages/setting-benefit.tsx
+++ pages/setting-benefit.tsx
@@ -1,6 +1,3 @@
-import Image from "next/image";
-
-
 export default function settingBenefit(){
 
     return (
@@ -9,11 +6,11 @@ export default function settingBenefit(){
                 <h2 className={'font-extrabold text-3xl mb-10'}>역할</h2>
                 <div className={'w-full '}>
                 <span>역할을 의사로 줬을때</span>
-                    <Image className={'mt-3  mx-auto'} src={'/static/images/has-role.png'} alt={'역할이 없을때'} width={1000} height={1000} priority/>
+                    <img className={'mt-3  mx-auto'} src={'/static/images/has-role.png'} alt={'역할이 없을때'} width={1000} height={1000} />
                 </div>
                 <div className={'w-full'}>
                 <span>역할을 아무것도 주지 않았을때</span>
-                <Image className={'mt-3  mx-auto'} src={'/static/images/has-no-role.png'} alt={'역할이 없을때'} width={1000} height={1000} priority/>
+                <img className={'mt-3  mx-auto'} src={'/static/images/has-no-role.png'} alt={'역할이 없을때'} width={1000} height={1000} />
                 </div>
             </div>
 
@@ -21,11 +18,11 @@ export default function settingBenefit(){
                 <h2 className={'font-extrabold text-3xl mb-10'}>패널티</h2>
                 <div className={'w-full'}>
                     <span>강력한 패널티를 줬을때</span>
-                    <Image className={'mt-3  mx-auto'} src={'/static/images/strong-penalty.png'} alt={'강력한 패널티'} width={1000} height={1000} priority/>
+                    <img className={'mt-3  mx-auto'} src={'/static/images/strong-penalty.png'} alt={'강력한 패널티'} width={1000} height={1000} />
                 </div>
                 <div className={'w-full'}>
                     <span>패널티를 마이너스로 줬을때</span>
-                    <Image className={'mt-3  mx-auto'} src={'/static/images/minus-penalty.png'} alt={'매우 약한 패널티'} width={1000} height={1000} priority/>
+                    <img className={'mt-3  mx-auto'} src={'/static/images/minus-penalty.png'} alt={'매우 약한 패널티'} width={1000} height={1000} />
                 </div>
             </div>
     </div>)
