4592c4996bd529d64fb27c2e45d63c513b52c8a6
wjdghks963 <chsw000@naver.com>
2023-03-27 22:23:53 +0900

    added :  - webview bridge 추가

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -63,18 +63,25 @@ export default function NavBar(){
                         </svg>
                         <span className={`text-center font-semibold`}>채팅</span>
                     </div>
-                    <div onClick={()=>goToPath('/profile')} className={`gap-2 cursor-pointer flex text-blue-200 hover:text-white mr-4`}>
-                        <svg className={'w-6'} fill="white" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"></path>
-                        </svg>
-                        <span className={`text-center font-semibold`}>프로필</span>
-                    </div>
                     <div onClick={()=>clearChats()} className={`gap-2 cursor-pointer flex text-blue-200 hover:text-white mr-4`}>
                         <svg className={'w-6'} fill="white" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
                             <path strokeLinecap="round" strokeLinejoin="round" d="M14.74 9l-.346 9m-4.788 0L9.26 9m9.968-3.21c.342.052.682.107 1.022.166m-1.022-.165L18.16 19.673a2.25 2.25 0 01-2.244 2.077H8.084a2.25 2.25 0 01-2.244-2.077L4.772 5.79m14.456 0a48.108 48.108 0 00-3.478-.397m-12 .562c.34-.059.68-.114 1.022-.165m0 0a48.11 48.11 0 013.478-.397m7.5 0v-.916c0-1.18-.91-2.164-2.09-2.201a51.964 51.964 0 00-3.32 0c-1.18.037-2.09 1.022-2.09 2.201v.916m7.5 0a48.667 48.667 0 00-7.5 0"></path>
                         </svg>
                         <span className={`text-center font-semibold`}>지우기</span>
                     </div>
+                    <div onClick={()=>goToPath('/profile')} className={`gap-2 cursor-pointer flex text-blue-200 hover:text-white mr-4`}>
+                        <svg className={'w-6'} fill="white" stroke="currentColor" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"></path>
+                        </svg>
+                        <span className={`text-center font-semibold`}>프로필</span>
+                    </div>
+                    <div onClick={()=>goToPath('/information-info')} className={`gap-2 cursor-pointer flex text-blue-200 hover:text-white mr-4`}>
+                        <svg className={'w-6'} fill="none" stroke="white" strokeWidth="1.5" viewBox="0 0 24 24" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
+                            <path strokeLinecap="round" strokeLinejoin="round" d="M11.25 11.25l.041-.02a.75.75 0 011.063.852l-.708 2.836a.75.75 0 001.063.853l.041-.021M21 12a9 9 0 11-18 0 9 9 0 0118 0zm-9-3.75h.008v.008H12V8.25z"></path>
+                        </svg>
+                        <span className={`text-center font-semibold`}>정보 및 문제</span>
+                    </div>
+
                 </div>
             </div>
         </div>
--- src/components/NavBarLayout.tsx
+++ src/components/NavBarLayout.tsx
@@ -1,15 +1,50 @@
 import NavBar from "./NavBar";
-import React from "react";
+import React, {useEffect, useState} from "react";
 import SEO from "./SEO/Head";
+import {isWebView} from "../utils/MobileBridge";
+import {useRouter} from "next/router";
+import { webViewRouteType} from "../../type";
+import {auth} from "../libs/firebase/auth";
+import {setToggle} from "../../store/modules/clearChats";
+import {useDispatch} from "react-redux";
 
 export default function NavBarLayout({children, styleAdd}:{children:React.ReactNode, styleAdd?:string}){
+    const router = useRouter();
+    const isMobile = isWebView();
+    const dispatch = useDispatch();
+
+    useEffect(()=>{
+        isMobile  ? (window as any).webviewNav = function(data:webViewRouteType) {
+            switch (data){
+                    case '':
+                    case 'profile':
+                    case 'information-info' :{
+                        return router.push(`/${data}`);
+                    }
+                    case 'chat' : {
+                        if(auth.currentUser !== null) {
+                            return router.push(`/${data}`);
+                        }else{
+                            return false;
+                        }
+                    }
+                    case 'delete' :{
+                        localStorage.removeItem('previousChats');
+                        return dispatch(setToggle())
+                    }
+                }
+
+
+        } : null
+
+    },[])
 
 
     return (
         <>
         <SEO title={'OA'}/>
         <main className={`h-screen flex flex-col`}>
-            <NavBar/>
+            {isMobile ? null: <NavBar/>}
             <div className={`max-h-screen flex-1 flex justify-center ${styleAdd}`}>
                 {children}
             </div>
