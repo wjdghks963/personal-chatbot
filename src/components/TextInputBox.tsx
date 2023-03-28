8a20502c51801368b60dcf7861eb2468dfc1df88
wjdghks963 <chsw000@naver.com>
2023-03-28 22:10:28 +0900

    modified :  - 확장성

--- src/components/TextInputBox.tsx
+++ src/components/TextInputBox.tsx
@@ -2,14 +2,16 @@ import {InputBoxProps} from "../../type";
 
 interface TextInputBox extends InputBoxProps {
     placeholder?:string;
+    inputStyleAdd? :string;
+    typeOf:string;
 }
 
-export default function TextInputBox<T>({propertyRef, identity, placeholder}:TextInputBox){
+export default function TextInputBox<T>({propertyRef, identity, placeholder,inputStyleAdd,typeOf}:TextInputBox){
 
     return (
         <div className={"py-3 flex justify-between"}>
             <span className={"my-auto align-middle"}>{identity}</span>
-            <input className="p-3 bg-inherit border-blue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:border-blue-600 focus:ring-blue-600" type={"text"} maxLength={10} ref={propertyRef} placeholder={`${placeholder}`}/>
+            <input className={`p-3 bg-inherit border-blue focus:outline-none focus:ring-2 focus:ring-offset-2 focus:border-blue-600 focus:ring-blue-600 ${inputStyleAdd}`} type={typeOf}  ref={propertyRef} placeholder={`${placeholder}`}/>
         </div>
     )
 
