8f8bd27bdd324757d255d9f82a3b58873a3c5d63
wjdghks963 <chsw000@naver.com>
2023-03-28 22:24:19 +0900

    modified :  - css 반응형 변경

--- src/components/LoginWithProvider.tsx
+++ src/components/LoginWithProvider.tsx
@@ -16,7 +16,6 @@ export default function LoginWithProvider({providerMap, providerKey,isConvert}:{
     return (
         <div className={`flex p-5 cursor-pointer border-2 border-black rounded-md shadow-md ${providerMap?.bgColor}`} onClick={onClick}>
             <Image className={'mr-5'} src={providerMap?.svg?.src as string } alt={`${providerKey}`} width={25} height={25}/>
-            <span className={"font-semibold"}>{providerKey}</span>
-            <span className={'ml-2'}> 시작하기</span>
+            <span className={"font-semibold"}>{providerKey} 시작하기</span>
     </div>)
 }
